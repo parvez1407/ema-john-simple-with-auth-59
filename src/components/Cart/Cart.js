6b170ddcd8c1d8e00bc2084bd627241a6ea06cc8
parvez1407 <info.nahidwd@gmail.com>
2022-10-06 22:21:07 +0600

    today 06 oct

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,24 +2,33 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({ cart }) => {
+    // console.log(cart);
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping;
+    }
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tax;
     return (
-        <div>
+        <div className='cart'>
             <h4 className='cart-title'>Order Summary</h4>
             <div className='cart-info'>
-                <p>Selected Items: {cart.length}</p>
-                <p>Total Price: ${ }</p>
-                <p>Total Shipping Charge: ${ }</p>
-                <p>Tax: ${ }</p>
+                <p>Selected Items: {quantity}</p>
+                <p>Total Price: ${total}</p>
+                <p>Total Shipping Charge: ${shipping}</p>
+                <p>Tax: ${tax.toFixed(2)}</p>
             </div>
-            <p className='total-cost'>Grand Total: { }</p>
+            <p className='total-cost'>Grand Total: {grandTotal.toFixed(2)}</p>
             <div>
                 <button>Clear Cart</button>
             </div>
             <div>
                 <button>Review Order</button>
             </div>
-
-
         </div>
     );
 };
